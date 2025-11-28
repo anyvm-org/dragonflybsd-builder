861bbd1e002889f53b21fcb5d0e89f701f861f6c
neil <gitpc@neilpang.com>
2025-11-28 22:29:18 +0100

    init

---
+++ hooks/postBuild.sh
@@ -0,0 +1,10 @@
+
+ln -s /usr/local/etc/ssl/cert.pem /etc/ssl/
+
+pkg install -y pkg
+
+cp /usr/local/etc/pkg/repos/df-latest.conf.sample /usr/local/etc/pkg/repos/df-latest.conf
+
+
+cat /boot/loader.conf
+
