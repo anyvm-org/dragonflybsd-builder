1ad892c9ea9fe859b461493f8e24e8909356e629
neil <gitpc@neilpang.com>
2025-12-07 18:11:21 +0100

    build fuse mod

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -5,6 +5,23 @@ pkg install -y pkg
 
 cp /usr/local/etc/pkg/repos/df-latest.conf.sample /usr/local/etc/pkg/repos/df-latest.conf
 
+echo 'autoboot_delay="1"' >> /boot/loader.conf
+echo 'beastie_disable="YES"' >> /boot/loader.conf
+
+#build fuse kernel mod
+cd /usr
+make src-create-shallow
+cd /usr/src/sys/vfs/fuse
+make
+make install
+rm -rf /usr/src/*
+echo 'fuse_load="YES"' >> /boot/loader.conf
+
+
 
 cat /boot/loader.conf
+
+
+boot0cfg -t 1 ad0
+
 
